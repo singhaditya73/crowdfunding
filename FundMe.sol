b515f597ebebb1de66d6d092a1d6f496f5d0de56
singhaditya73 <adityasinghrajput755@gmail.com>
2025-07-01 23:53:27 +0530

    initilize the Fund contract

--- FundMe.sol
+++ FundMe.sol
@@ -3,4 +3,16 @@
 // set a minimum funding value in USD
 
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.19;
+
+contract FundMe {
+    function fund() public {
+        
+
+    }
+
+    function withdraw() public { 
+
+
+    }
+}
