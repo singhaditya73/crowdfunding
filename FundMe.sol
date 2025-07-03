3e0b930c7b27cb72e13ce0f02d65dc664d39887b
singhaditya73 <adityasinghrajput755@gmail.com>
2025-07-04 02:33:31 +0530

    added functionality to withdraw function

--- FundMe.sol
+++ FundMe.sol
@@ -8,6 +8,12 @@ contract FundMe {
     uint256 public minimumUsd = 5e18;
     address[] public funders;
     mapping (address funders => uint256 amountFunded) public addressToAmountFunded;
+    address public owner;
+    
+    constructor() {
+        owner  = msg.sender;
+    }
+
     function fund() public payable  {
           require(msg.value.getConversionRate() >= minimumUsd, "didnt send enough eth");
           funders.push(msg.sender);
@@ -15,8 +21,19 @@ contract FundMe {
     }
     
 
-    function withdraw() public { 
+    function withdraw() public onlyOwner{ 
+        for(uint256 fundersIndex = 0; fundersIndex < funders.length; fundersIndex++){
+           address funder = funders[fundersIndex];
+           addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}(""); 
+        require(callSuccess, "Call Failed");
     }
 
-    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the right person to use this");
+        _;
+    }
 }
