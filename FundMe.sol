332a23f9b8c0907a94720e38b0d0f594be98ee30
singhaditya73 <adityasinghrajput755@gmail.com>
2025-07-04 04:01:05 +0530

    done

--- FundMe.sol
+++ FundMe.sol
@@ -11,49 +11,53 @@ contract FundMe {
     using PriceConverter for uint256;
     uint256 public constant minimumUsd = 5e18;
     address[] public funders;
-    mapping (address funders => uint256 amountFunded) public addressToAmountFunded;
+    mapping(address => uint256) public addressToAmountFunded;
     address public immutable i_owner;
-    
+
     constructor() {
-        i_owner  = msg.sender;
+        i_owner = msg.sender;
     }
 
-    function fund() public payable  {
-          //require(msg.value.getConversionRate() >= minimumUsd, "didnt send enough eth");
-          if(msg.value.getConversionRate()< minimumUsd){
+    function fund() public payable {
+        //require(msg.value.getConversionRate() >= minimumUsd, "didnt send enough eth");
+        if (msg.value.getConversionRate() < minimumUsd) {
             revert InsufficientEth();
-          }
-          funders.push(msg.sender);
-          addressToAmountFunded[msg.sender] += msg.value;
+        }
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
     }
-    
 
-    function withdraw() public onlyOwner{ 
+    modifier onlyOwner() {
+        //require(msg.sender == i_owner, "Not the right person to use this");
+        if (msg.sender != i_owner) {
+            revert notOwner();
+        }
+        _;
+    }
+
+    function withdraw() public onlyOwner {
         //for(uint256 fundersIndex = 0; fundersIndex < funders.length; fundersIndex++){
-            uint256 length = funders.length;
-            for (uint256 i = 0; i < length; i++){
-           //address funder = funders[fundersIndex];
-           address funder = funders[i];
-           addressToAmountFunded[funder] = 0;
+        uint256 length = funders.length;
+        for (uint256 i = 0; i < length; i++) {
+            //address funder = funders[fundersIndex];
+            address funder = funders[i];
+            addressToAmountFunded[funder] = 0;
         }
         funders = new address[](0);
 
-        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}(""); 
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
         if (!callSuccess) {
             revert TransferFailed();
         }
     }
 
-    modifier onlyOwner() {
-        //require(msg.sender == i_owner, "Not the right person to use this");
-        if(msg.sender != i_owner) {revert notOwner();}
-        _;
-    }
-
-    receive() external payable { 
+    receive() external payable {
         fund();
     }
-    fallback() external payable { 
+
+    fallback() external payable {
         fund();
     }
 }
