9be0a2c18fb4fca89bbac9f3d2cf20253f9d8d94
singhaditya73 <adityasinghrajput755@gmail.com>
2025-07-04 01:42:16 +0530

    Refactor: Moved getConversionRate and related logic to PriceConverter library to reduce verbosity in main contract

--- FundMe.sol
+++ FundMe.sol
@@ -1,29 +1,22 @@
-// get funds from user
-// withdraw funds
-// set a minimum funding value in USD
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
+    uint256 public minimumUsd = 5e18;
+    address[] public funders;
+    mapping (address funders => uint256 amountFunded) public addressToAmountFunded;
     function fund() public payable  {
-          require(msg.value >= 1e18, "didnt send eenough eth");
+          require(msg.value.getConversionRate() >= minimumUsd, "didnt send enough eth");
+          funders.push(msg.sender);
+          addressToAmountFunded[msg.sender] += msg.value;
     }
+    
 
     function withdraw() public { 
     }
 
-    function getPrice() public view returns (uint256) {
-        // address  0x694AA1769357215DE4FAC081bf1f309aDC325306
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int256 answer,,,)= priceFeed.latestRoundData();
-        return uint256(answer * 1e10);
-    }
-    function getConversionRate(uint256 ethAmount) public view returns (uint256){
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
-    }
+    
 }
